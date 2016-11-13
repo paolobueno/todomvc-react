a00210c0b36527aa556062f86a754a8f35a6e8ce
Paolo Bueno <paolohaji@gmail.com>
2016-11-13 12:33:15 +0000

    Add todo count display

--- js/components/App.jsx
+++ js/components/App.jsx
@@ -16,22 +16,25 @@ export default class App extends React.Component {
             ]
         };
     }
+    todoCount() {
+        return this.state.todos.length;
+    }
     render() {
         return <section className="todoapp">
             <header className="header">
 				<h1>todos</h1>
 				<input className="new-todo" placeholder="What needs to be done?" />
 			</header>
-			<section class="main">
+			<section className="main">
 				<input id="toggle-all" className="toggle-all" type="checkbox" />
 				<label htmlFor="toggle-all">Mark all as complete</label>
 				<TodoList todos={this.state.todos} />
 			</section>
 			<footer className="footer">
-				<span className="todo-count"><strong>0</strong> item left</span>
+				<span className="todo-count"><strong>{this.todoCount()}</strong> item left</span>
 				<ul className="filters">
 					<li>
-						<a class="selected" href="#/">All</a>
+						<a className="selected" href="#/">All</a>
 					</li>
 					<li>
 						<a href="#/active">Active</a>
